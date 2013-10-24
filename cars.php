4b0a8e680927302d6319d2e09160042c12c3ad7a
Ben <lumezious@googlemail.com>
2013-10-24 16:30:43 +0100

    Added a property that holds the GET colour
    Added a property called '$colour' that holds the GET colour. Echo it to
    show its working (will be removed later).

--- cars.php
+++ cars.php
@@ -27,7 +27,6 @@
 	
 	<!-- DO NOT USE THIS UNTIL PART 2 -->
 	<!-- when told to, remove these comments -->
-	<!--
 	<div class="row">
 		<p class="col-md-2">Car Colour</p>
 		<form action="cars.php" method="GET">
@@ -44,7 +43,10 @@
 		
 		<p><a href="cars.php" class="btn btn-primary btn-sm col-md-offset-1">Reset Colour</a></p>
 	</div>
-	-->
+	<?php
+		$colour = $_GET['colour'];
+		echo $colour;
+	?>
 	<!-- /part 2 code -->
 	
 	
