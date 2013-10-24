779689829b153ad6c7286e57bb3df9c29d585132
Ben <lumezious@googlemail.com>
2013-10-24 15:07:00 +0100

    Relocated part e. code to the correct place
    Relocated part e. code to the correct place and removed the placeholder.

--- cars.php
+++ cars.php
@@ -25,11 +25,6 @@
 	
 	<p>View our wide range of fabulous cars and low low prices, and quality. Ensure your car is only sold by a Boyd approved dealership.</p>
 	
-	<?php 
-		include('data/carData.php'); 
-		print_r($carData);
-	?>
-	
 	<!-- DO NOT USE THIS UNTIL PART 2 -->
 	<!-- when told to, remove these comments -->
 	<!--
@@ -56,9 +51,10 @@
 	<!-- car list -->
 	<div id="car-list" class="row">
 		
-		<!-- =========================== -->
-		<!-- PART 1 CAR LIST PLACEHOLDER -->
-		<!-- =========================== -->
+	<?php 
+		include('data/carData.php'); 
+		print_r($carData);
+	?>
 		
 	</div><!-- /car list -->
 	
